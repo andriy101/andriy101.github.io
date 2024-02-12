fa77aa70cbb513bae0dddadba48def7140b93b2d
Andriy Malish <andriymalish@Andriys-MacBook-Pro.local>
2024-02-12 22:14:54 +0200

    commit app js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import { MarkerClusterer } from 'https://cdn.skypack.dev/@googlemaps/markerclusterer@2.3.1';
 import jsonAll from './all.js';
 
-async function initMap() {
+async function initMap(data) {
+  console.log('+++++ DATA', data.at(0));
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary('maps');
 
@@ -9,7 +10,7 @@ async function initMap() {
   const map = new google.maps.Map(document.getElementById('map'));
 
   // Add some markers to the map.
-  const markers = capitals.map(position => {
+  const markers = data.map(position => {
     const marker = new google.maps.Marker({
         position,
         icon: {
@@ -36,4 +37,31 @@ const capitals = Object.values(jsonAll.Results).flatMap(data => data.Capital ? {
     lng: data.Capital.GeoPt.at(1)
 } : []);
 
-initMap();
+console.log('+++ CAPS', capitals.at(0));
+
+const getMarkers = (code, totalMarkers) => fetch(`/coordinates/${code}.json`)
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    console.error(`[APP] Failed to fetch markers for ${code}`);
+    return [];
+  })
+  .then(res => res.slice(0, totalMarkers));
+
+fetch('https://b.primefactorgames.com/steps/get-all')
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    console.error('[APP] Failed to fetch GET ALL');
+    return [];
+  })
+  .then(res => {
+    console.log('+++ RRR', res);
+    return Promise.all(
+      res.filter(({ totalMarkers }) => totalMarkers).map(({ countryCode, totalMarkers }) => getMarkers(countryCode, totalMarkers))
+    );
+  })
+  .then(res => res.flatMap(i => i))
+  .then(data => initMap(data));
