40983bd1b5832b611edc4a4161ff81a1dfa93584
Andriy Malish <andriymalish@Andriys-MacBook-Pro.local>
2025-02-23 15:24:15 +0200

    SR 2

--- sr/script.js
+++ sr/script.js
@@ -1,5 +1,8 @@
 // https://www.w3.org/TR/wai-aria/states_and_properties#aria-live
 document.querySelector('button').addEventListener('click', () => {
-  document.querySelector('.cdk-visually-hidden').textContent =
-    document.querySelector('input[type=text]').value || 'Hello WORLD';
+  document.querySelector('.cdk-visually-hidden').textContent = '';
+  setTimeout(() => {
+    document.querySelector('.cdk-visually-hidden').textContent =
+      document.querySelector('input[type=text]').value || 'Hello WORLD';
+  }, 100);
 });
